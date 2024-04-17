ff7c8cc19c9d51871a4b73af2e17a18562c3f393
thisisnago <yurii.nagorniak@gmail.com>
2024-04-17 19:19:57 +0100

    CRA

---
+++ src/components/NewTaskForm.tsx
@@ -0,0 +1,22 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Task } from "../models/task.model";
+
+
+const NewTaskForm = () => {
+    const {
+        register,
+        handleSubmit
+    } = useForm<Task>();
+
+    const onSubmit: SubmitHandler<Task> = (data) => console.log(data);
+
+    return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" {...register("title", { required: true })} />
+        <input type="text" {...register("description")} />
+        <input type="submit" />
+      </form>
+    );
+}
+
+export default NewTaskForm;
