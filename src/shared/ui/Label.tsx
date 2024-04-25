251d94757b3ab61f4da700f32dec697406d47fc5
thisisnago <yurii.nagorniak@gmail.com>
2024-04-25 21:44:30 +0100

    Deleted console logs in the Label

--- src/shared/ui/Label.tsx
+++ src/shared/ui/Label.tsx
@@ -6,7 +6,5 @@ interface LabelProps {
 }
 
 export const Label = forwardRef<HTMLLabelElement, LabelProps>(({ label, htmlFor, ...rest }, ref): JSX.Element => {
-    console.log(label);
-    console.log(ref);
     return <label ref={ref} htmlFor={htmlFor} className="block text-gray-700 text-sm font-bold mb-2" {...rest}>{label}</label>;
 });
