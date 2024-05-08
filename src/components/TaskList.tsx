0c7b81ef74ab520ad5eaad310572a2019bb46351
thisisnago <yurii.nagorniak@gmail.com>
2024-05-08 23:16:25 +0100

    Added task info button

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -232,11 +232,40 @@ const TaskList = ({
                             color={listData.color}
                             completed={task.completed}
                         />
-                        <h3 className="w-[70%] border-2 text-left ml-2 truncate">{task.title}</h3>
-                        <div className="flex flex-row gap-2">
-                            {task.description && (
-                                <div>
-                                    <button onClick={taskInfoHandleClick}>Info</button>
+                            </div>
+                            <h3 className="w-4/5 sm:w-[80%] text-left pl-2 truncate">
+                                {task.title}
+                            </h3>
+                        </div>
+                        <div className="flex items-center justify-end gap-2 w-1/5 sm:w-12 pr-2">
+                            {/* {task.description && ( */}
+                                <div className="flex items-center">
+                                    <button className="w-5 h-5" onClick={taskInfoHandleClick}>
+                                        <svg
+                                            fill={listData.color}
+                                            height="100%"
+                                            width="100%"
+                                            version="1.1"
+                                            id="Capa_1"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 330 330"
+                                        >
+                                            <g>
+                                                <path
+                                                    d="M165,0C74.019,0,0,74.02,0,165.001C0,255.982,74.019,330,165,330s165-74.018,165-164.999C330,74.02,255.981,0,165,0z
+		 M165,300c-74.44,0-135-60.56-135-134.999C30,90.562,90.56,30,165,30s135,60.562,135,135.001C300,239.44,239.439,300,165,300z"
+                                                />
+                                                <path
+                                                    d="M164.998,70c-11.026,0-19.996,8.976-19.996,20.009c0,11.023,8.97,19.991,19.996,19.991
+		c11.026,0,19.996-8.968,19.996-19.991C184.994,78.976,176.024,70,164.998,70z"
+                                                />
+                                                <path
+                                                    d="M165,140c-8.284,0-15,6.716-15,15v90c0,8.284,6.716,15,15,15c8.284,0,15-6.716,15-15v-90C180,146.716,173.284,140,165,140z
+		"
+                                                />
+                                            </g>
+                                        </svg>
+                                    </button>
                                     <Popover
                                         id={taskInfoId}
                                         open={taskInfoOpen}
