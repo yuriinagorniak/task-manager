db5581246010b13c17f4b07fb950e10fc28f23db
thisisnago <yurii.nagorniak@gmail.com>
2024-05-14 23:44:20 +0100

    Removed list item in the task list (changed by TaskItem component)

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -178,54 +178,6 @@ const TaskList = ({
             <ul className="py-2 flex flex-col items-center">
                 {uncompletedTasks.map((task) => (
                     <TaskItem task={task} listColor={listData.color} completeTask={completeTask} deleteTask={deleteTask} />
-                    // <li
-                    //     key={task.id}
-                    //     className="flex items-center my-1 justify-between w-[90%]"
-                    //     style={{ color: task.completed ? "#6e6e6e" : "#000000" }}
-                    // >
-                    //     <div className="flex items-center w-4/5 sm:w-full ">
-                    //         <div className="w-5">
-                    //             <TickButton
-                    //                 onClick={completeTask.bind(null, task.id)}
-                    //                 color={listData.color}
-                    //                 completed={task.completed}
-                    //             />
-                    //         </div>
-                    //         <h3 className="w-4/5 sm:w-[80%] text-left pl-2 truncate">
-                    //             {task.title}
-                    //         </h3>
-                    //     </div>
-                    //     <div className="flex items-center justify-end gap-2 w-1/5 sm:w-12 pr-2">
-                    //         <div className="flex items-center">
-                    //             <TaskDescriptionPopover
-                    //                 listColor={listData.color}
-                    //                 taskTitle={task.title}
-                    //                 taskDescription={task.description}
-                    //             />
-                    //         </div>
-
-                    //         <div>
-                    //             <button
-                    //                 className="w-5 h-5 flex items-center"
-                    //                 title="Delete the task"
-                    //                 onClick={deleteTask.bind(null, task.id)}
-                    //             >
-                    //                 <svg
-                    //                     width="100%"
-                    //                     height="100%"
-                    //                     viewBox="0 0 24 24"
-                    //                     xmlns="http://www.w3.org/2000/svg"
-                    //                 >
-                    //                     <g fill={listData.color} fill-rule="nonzero">
-                    //                         <path d="m12 22c5.5228475 0 10-4.4771525 10-10s-4.4771525-10-10-10-10 4.4771525-10 10 4.4771525 10 10 10zm0 2c-6.627417 0-12-5.372583-12-12s5.372583-12 12-12 12 5.372583 12 12-5.372583 12-12 12z" />
-                    //                         <path d="m9.20710678 16.2071068c-.39052429.3905243-1.02368927.3905243-1.41421356 0s-.39052429-1.0236893 0-1.4142136l6.99999998-6.99999998c.3905243-.39052429 1.0236893-.39052429 1.4142136 0s.3905243 1.02368927 0 1.41421356z" />
-                    //                         <path d="m7.79289322 9.20710678c-.39052429-.39052429-.39052429-1.02368927 0-1.41421356s1.02368927-.39052429 1.41421356 0l7.00000002 6.99999998c.3905243.3905243.3905243 1.0236893 0 1.4142136s-1.0236893.3905243-1.4142136 0z" />
-                    //                     </g>
-                    //                 </svg>
-                    //             </button>
-                    //         </div>
-                    //     </div>
-                    // </li>
                 ))}
             </ul>
 
@@ -238,21 +190,6 @@ const TaskList = ({
                     <ul className="py-2 flex flex-col items-center">
                         {completedTasks.map((task) => (
                             <TaskItem task={task} listColor={listData.color} completeTask={completeTask} deleteTask={deleteTask} />
-                            // <li
-                            //     key={task.id}
-                            //     className="flex items-center justify-around my-1"
-                            //     style={{ color: task.completed ? "#6e6e6e" : "#000000" }}
-                            // >
-                            //     <TickButton
-                            //         onClick={completeTask.bind(null, task.id)}
-                            //         color={listData.color}
-                            //         completed={task.completed}
-                            //     />
-                            //     <h3>{task.title}</h3>
-                            //     {task.description && <span>{task.description}</span>}
-                            //     <button>Info</button>
-                            //     <button onClick={deleteTask.bind(null, task.id)}>Del</button>
-                            // </li>
                         ))}
                     </ul>
                 </div>
