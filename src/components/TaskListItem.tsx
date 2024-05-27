5c66709037ff9c0bcf94f4d547a530c106f40988
thisisnago <yurii.nagorniak@gmail.com>
2024-05-27 23:58:30 +0100

    Added TaskListItem component

---
+++ src/components/TaskListItem.tsx
@@ -0,0 +1,25 @@
+import { TaskElement } from "../shared/ui/TaskIElement";
+import { Task } from "../models/task.model";
+import { Color } from "../models/color.model";
+
+interface TaskListItemProps {
+    tasks: Task[];
+    listColor: Color;
+    completeTask: (id: string) => void;
+    deleteTask: (id: string) => void;
+}
+
+export const TaskListItem = ({ tasks, listColor, completeTask, deleteTask }: TaskListItemProps) => {
+    return (
+        <ul className="my-2 flex flex-col items-center">
+            {tasks.map((task) => (
+                <TaskElement
+                    task={task}
+                    listColor={listColor}
+                    completeTask={completeTask}
+                    deleteTask={deleteTask}
+                />
+            ))}
+        </ul>
+    );
+};
