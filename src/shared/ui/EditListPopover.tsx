45817b3790be7b904f6e32d0ee5cdda39202f16f
thisisnago <yurii.nagorniak@gmail.com>
2024-05-27 23:56:39 +0100

    Added EditListPopover

---
+++ src/shared/ui/EditListPopover.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { getContrastColor } from "../../utils/getContrastColor";
+import { editListAnchorType } from "../../components/TaskEdit";
+import { Color } from "../../models/color.model";
+import Popover from "@mui/material/Popover";
+import React from "react";
+
+interface EditListPopoverProps extends React.PropsWithChildren {
+    editListAnchor: editListAnchorType;
+    setEditListAnchor: (anchor: editListAnchorType) => void;
+    buttonColor: Color;
+}
+
+export const EditListPopover = ({
+    children,
+    editListAnchor,
+    setEditListAnchor,
+    buttonColor,
+}: EditListPopoverProps): JSX.Element => {
+    
+    const editListHandleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setEditListAnchor(event.currentTarget);
+    };
+
+    const editListHandleClose = () => {
+        setEditListAnchor(null);
+    };
+
+    const editListOpen = Boolean(editListAnchor);
+    const editListId = editListOpen ? "edit-list-popover" : undefined;
+
+    return (
+        <>
+            <button
+                style={{ color: getContrastColor(buttonColor) }}
+                onClick={editListHandleClick}
+                title="Options"
+            >
+                ⠇
+            </button>
+            <Popover
+                anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                }}
+                transformOrigin={{
+                    vertical: "bottom",
+                    horizontal: "left",
+                }}
+                id={editListId}
+                open={editListOpen}
+                anchorEl={editListAnchor}
+                onClose={editListHandleClose}
+            >
+                {children}
+            </Popover>
+        </>
+    );
+};
