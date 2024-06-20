5edfe330ca1ed2704e7280937bc294b07c44d603
thisisnago <yurii.nagorniak@gmail.com>
2024-06-21 00:01:01 +0100

    feat: define the type of data in useTasks

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
+import { AppData } from "../models/appData.model";
 
 export const useTasks = () => {
-    const [data, setData] = useState();
+    const [data, setData] = useState<AppData>();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState();
     
