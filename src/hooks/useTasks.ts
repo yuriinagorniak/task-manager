93c10b90429e18d21cf0dc8b52d7067768a31758
thisisnago <yurii.nagorniak@gmail.com>
2024-06-25 23:55:32 +0100

    feat: add axios to useTasks for data fetching

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
 import { AppData } from "../models/appData.model";
+import { TASK_URL } from "../urls";
+import axios from "axios";
 
 export const useTasks = () => {
     const [data, setData] = useState<AppData>();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState();
+
     
     useEffect(() => {
-      
+      axios.get(TASK_URL).then((response) => console.log(response)).catch((error) => console.log(error));
     }, []);
   
     return { data, loading, error };
