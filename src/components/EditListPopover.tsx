f11636e03fde5b9485adf02a216bcd3e2daaa55b
thisisnago <yurii.nagorniak@gmail.com>
2024-07-06 23:49:33 +0100

    fix: fix minor bugs

--- src/components/EditListPopover.tsx
+++ src/components/EditListPopover.tsx
@@ -1,5 +1,5 @@
 import { getContrastColor } from "../utils/getContrastColor";
-import { editListAnchorType } from "./TaskEdit";
+import { editListAnchorType } from "./EditList";
 import { Color } from "../models/color.model";
 import Popover from "@mui/material/Popover";
 import React from "react";
