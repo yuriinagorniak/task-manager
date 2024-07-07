cd1bece9c249c10d89693664e91b3bf43eca54ec
thisisnago <yurii.nagorniak@gmail.com>
2024-07-07 18:00:44 +0100

    feat: add NewItemForm component with new list and task forms

---
+++ src/components/NewItemForm.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import NewTaskForm from "./NewTaskForm";
+import NewListForm from "./NewListForm";
+import { FormTabButton } from "../shared/ui/FormTabButton";
+import { Task } from "../models/task.model";
+import { List } from "../models/list.model";
+
+interface newItemFormProps {
+    tasks: Task[];
+    lists: List[];
+    addNewTask: (task: Task) => void;
+    addNewList: (list: List) => void;
+}
+
+export const NewItemForm = ({ tasks, lists, addNewTask, addNewList }: newItemFormProps) => {
+    const [newTaskFormDisplayed, setNewTaskFormDisplayed] = useState<boolean>(true);
+
+    return (
+        <div className="m-2 overflow-hidden shadow-lg appearance-none border-[3px] rounded-md border-[#d8d9dd] bg-white text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+            <div className="flex justify-between text-lg">
+                <FormTabButton
+                    isNewTaskTab={true}
+                    tabDisplayed={newTaskFormDisplayed}
+                    setTabDisplayed={setNewTaskFormDisplayed}
+                />
+                <FormTabButton
+                    isNewTaskTab={false}
+                    tabDisplayed={!newTaskFormDisplayed}
+                    setTabDisplayed={setNewTaskFormDisplayed}
+                />
+            </div>
+
+            <div className="flex items-center justify-center h-64">
+                {newTaskFormDisplayed ? (
+                    <NewTaskForm lists={lists} addNewTask={addNewTask} />
+                ) : (
+                    <NewListForm addNewList={addNewList} />
+                )}
+            </div>
+        </div>
+    );
+};
